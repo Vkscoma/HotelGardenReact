dc30be0ba7fe166d17d42854378db732cfc8fdef
Vkscoma <vkscoma@gmail.com>
2024-05-22 14:48:39 -0700

    final touches (praying)

--- hotel-garden-main/src/components/Hero.jsx
+++ hotel-garden-main/src/components/Hero.jsx
@@ -6,14 +6,14 @@ const Hero = () => {
     const { scrollY } = useScroll();
 
     // Create a parallax effect by transforming the scrollY value
-    const y = useTransform(scrollY, [0, 300], [0, 200]); // Adjust these values as needed for your effect
+    const y = useTransform(scrollY, [0, 300], [0, 100]); // Adjust these values as needed for your effect
 
     const titleStyle = {
         textShadow: '0 0 10px rgba(255, 255, 255, 0.7)',
     };
 
     return (
-        <div id='home' className='relative w-full h-screen'>
+        <div id='home' className='relative w-full h-full'>
             <div className="relative flex justify-center md:mt-40 my-64">
                 <motion.h1
                     ref={titleRef}
