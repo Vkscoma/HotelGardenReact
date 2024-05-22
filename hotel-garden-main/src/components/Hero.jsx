904f1865a0b3964aa4db1725b29998abb0187f1a
Vkscoma <vkscoma@gmail.com>
2024-05-22 15:09:51 -0700

    Last push - added opac to title on scroll

--- hotel-garden-main/src/components/Hero.jsx
+++ hotel-garden-main/src/components/Hero.jsx
@@ -7,6 +7,7 @@ const Hero = () => {
 
     // Create a parallax effect by transforming the scrollY value
     const y = useTransform(scrollY, [0, 300], [0, 100]); // Adjust these values as needed for your effect
+    const opacity = useTransform(scrollY, [0, 300], [1, 0.4]); // Adjust these values as needed for your effect
 
     const titleStyle = {
         textShadow: '0 0 10px rgba(255, 255, 255, 0.7)',
@@ -18,7 +19,7 @@ const Hero = () => {
                 <motion.h1
                     ref={titleRef}
                     className="md:text-9xl mobile--title text-white text-center font-bold"
-                    style={{ ...titleStyle, y }}
+                    style={{ ...titleStyle, y, opacity }}
                 >
                     HOTEL <br />GARDEN
                 </motion.h1>
