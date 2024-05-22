f4ae3434647b517154da3ebc270c37d50553cda8
Vkscoma <vkscoma@gmail.com>
2024-05-22 08:12:18 -0700

    Update App.jsx

--- hotel-garden-main/src/App.jsx
+++ hotel-garden-main/src/App.jsx
@@ -1,7 +1,7 @@
 import './index.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import HeroBackground from './assets/HeorBackground.json'
-const Lottie = () => import('lottie-react')
+import Lottie from 'lottie-react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Music from './components/Music'
